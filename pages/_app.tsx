7c96872fe8bfb30110e7de67b426419eca080007
k0d1kk <leonmiy07@gmail.com>
2023-06-03 13:12:13 +0300

    correct app tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { GlobalStyle } from '@/styles/global';
+import type { AppProps } from 'next/app';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+    secondary: '#000000',
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
