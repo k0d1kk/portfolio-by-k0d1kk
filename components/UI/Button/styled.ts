a6c422c2cbdc55425b4760030d1eeccec4d6ee65
k0d1kk <leonmiy07@gmail.com>
2023-07-07 12:57:28 +0300

    some changes

--- components/UI/Button/styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-import { IButton } from './types';
-import { theme } from '@/styles/global';
-
-export const ButtonStyled = styled.button<IButton>``;
--- components/UI/Button/types.ts
+++
@@ -1,5 +0,0 @@
-import { ButtonHTMLAttributes, ReactNode } from 'react';
-
-export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: ReactNode;
-}
